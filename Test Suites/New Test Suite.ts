adb2d7044dd235962bd5d9ba7348f755f360af72
Sebastian <ingesql1992@hotmail.com>
2020-11-12 16:07:35 -0500

    Added katalon

---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>82ff03ec-05a2-4750-b8d9-d6921311fe7e</testSuiteGuid>
+   <testCaseLink>
+      <guid>272c1cb3-7256-4446-bb62-8e2d8a7901e5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/New Test Case</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
