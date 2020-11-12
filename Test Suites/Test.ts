e179ff154787b29df38ffac30731c03b49d8daed
Sebastian <ingesql1992@hotmail.com>
2020-11-12 16:45:20 -0500

    Changed names

--- Test Suites/Test.ts
+++ Test Suites/Test.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>Test Suite</name>
+   <name>Test</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
